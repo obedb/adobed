b9559f98ed362211b99c159640dac79b15ebd2f2
Karoli Kolokonyi <karoli@kepler.org>
2017-03-09 13:59:10 +0200

    added the session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 get '/welcome', to: 'pages#welcome'
 
+get "/login", to: 'sessions#new'
+post "/login", to: 'sessions#create'
+delete "/logout", to: 'sessions#destroy'
+
+
 end
